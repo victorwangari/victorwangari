5ce4c490ab483834fd9b23a2d4584830f1ab83f3
root <root@VICKY-DESKTOP-2CS1R7B.localdomain>
2025-01-15 21:06:12 +0300

    all changes

--- src/components /contact .jsx
+++ src/components /contact .jsx
@@ -1,41 +1,43 @@
 import React from "react";
+import '../CSS/contact.css'
 
 
 function Contact() {
-    return (
-        
-        <div class="contact bg-black">
-            <div class="row">
-                <div class="contact-letf">
-                    <h1 class="sub-title">Contact Me</h1>
-                    <p><i class="fa-solid fa-paper-plane"></i>victorwangari65187@gmail.com</p>
-                    <p><i class="fa-solid fa-phone"></i>+254115314344</p>
-                    <div class="social-icon">
-                        <a href=""><i class="fa-brands fa-facebook"></i></a>
-                        <a href=""><i class="fa-brands fa-square-instagram"></i></a>
-                        <a href=""><i class="fa-brands fa-square-whatsapp"></i></a>
-                        <a href=""><i class="fa-brands fa-x-twitter"></i></a>
-                        <a href=""><i class="fa-brands fa-telegram"></i></a>
+    return(
+       <>
+        <div id="contact" className="bg-black">
+        <div className="contact">
+            <div className="row">
+                <div className="contact-left col-4">
+                    <h1 className="sub-title text-white">Contact <span className="span-skill">Me</span></h1>
+                    <p className="text-white fs-5"><img className="phone" src="https://img.icons8.com/?size=100&id=9rAH3aJTe5Vu&format=png&color=000000" alt="G-mail" /> victorwangari65187@gmail.com</p>
+                    <p className="text-white"><img className="phone" src="https://img.icons8.com/?size=100&id=WV326xpsBMyb&format=png&color=000000" alt="Phone" />  +254115314344</p>
+                    <div className="social-icon">
+                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" alt="Lineked In" /></a>
+                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=A1JUR9NRH7sC&format=png&color=000000" alt="WhatsApp" /></a>
+                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=BrU2BBoRXiWq&format=png&color=000000" alt="Instagram" /></a>
+                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=3veRWJpxPPDH&format=png&color=000000" alt="TikTok" /></a>
                     </div>
-                    <a href="./img/file 1.pdf" download class="btn btn2">Download Cv</a>
+                    <a href="./img/file 1.pdf" download className="btn btn2 bg-warning">Download Cv</a>
 
                 </div>
-                <div class="contact-right">
+                <div className="contact-right col-6 ">
                     <form name="submit-to-google-sheet">
                         <input type="text" name="Name" placeholder="Your Name " required/>
                         <input type="email" name="Email" placeholder="Your Email" required/>
                         <textarea name="Message" rows="6" placeholder="Your Message"></textarea>
-                        <button type="submit" class="btn btn2">Submit</button>
+                        <button type="submit" className="btn btn2 bg-warning">Submit</button>
                     </form>
                     <span id="msg"></span>
                 </div>
             </div>
-            <div class="copyright">
-            <p>Copyrihgt <i class="fa-regular fa-copyright"></i> 2024 VICTOR. all rights reserved.</p>
         </div>
+        <div className="copyright">
+            <p className="text-white">Copyrihgt <img className="text-white" src="https://img.icons8.com/?size=100&id=cBtsUdDPCDm3&format=png&color=000000" alt="" /> 2024 VICTOR. all rights reserved.</p>
         </div>
-       
-   
+    </div>
+       </>
+    
     )
 }
 
