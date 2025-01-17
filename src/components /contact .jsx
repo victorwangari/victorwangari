b294257a979cb3df60036c2e5042f8280e3e5491
root <root@VICKY-DESKTOP-2CS1R7B.localdomain>
2025-01-17 17:24:53 +0300

    update app

--- src/components /contact .jsx
+++ src/components /contact .jsx
@@ -4,40 +4,29 @@ import '../CSS/contact.css'
 
 function Contact() {
     return(
-       <>
-        <div id="contact" className="bg-black">
-        <div className="contact">
-            <div className="row">
-                <div className="contact-left col-4">
-                    <h1 className="sub-title text-white">Contact <span className="span-skill">Me</span></h1>
-                    <p className="text-white fs-5"><img className="phone" src="https://img.icons8.com/?size=100&id=9rAH3aJTe5Vu&format=png&color=000000" alt="G-mail" /> victorwangari65187@gmail.com</p>
-                    <p className="text-white"><img className="phone" src="https://img.icons8.com/?size=100&id=WV326xpsBMyb&format=png&color=000000" alt="Phone" />  +254115314344</p>
-                    <div className="social-icon">
-                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" alt="Lineked In" /></a>
-                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=A1JUR9NRH7sC&format=png&color=000000" alt="WhatsApp" /></a>
-                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=BrU2BBoRXiWq&format=png&color=000000" alt="Instagram" /></a>
-                        <a href=""><img className="icon" src="https://img.icons8.com/?size=100&id=3veRWJpxPPDH&format=png&color=000000" alt="TikTok" /></a>
-                    </div>
-                    <a href="./img/file 1.pdf" download className="btn btn2 bg-warning">Download Cv</a>
-
+        <div className="bg-black">
+            <div className="contact">
+                <h2 className="text-white">CONTACT <span className="span-skill">ME</span></h2> 
+            </div>
+            <div className=" contact-details">
+                <div>
+                    <h4 className="text-white fs-5"><img className="email" src="https://img.icons8.com/?size=100&id=P7UIlhbpWzZm&format=png&color=000000" alt="" /> victorwangari65187@gmail.com</h4>
                 </div>
-                <div className="contact-right col-6 ">
-                    <form name="submit-to-google-sheet">
-                        <input type="text" name="Name" placeholder="Your Name " required/>
-                        <input type="email" name="Email" placeholder="Your Email" required/>
-                        <textarea name="Message" rows="6" placeholder="Your Message"></textarea>
-                        <button type="submit" className="btn btn2 bg-warning">Submit</button>
-                    </form>
-                    <span id="msg"></span>
+                <div>
+                    <h4 className="text-white"><img className="email"  src="https://img.icons8.com/?size=100&id=WV326xpsBMyb&format=png&color=000000" alt="" />+254115314344</h4>
+                </div>
+                <div className="social-media">
+                    <a href=""><img className="ICONS" src="https://img.icons8.com/?size=100&id=114445&format=png&color=000000" alt="" /></a>
+                    <a href=""><img className="ICONS" src="https://img.icons8.com/?size=100&id=16713&format=png&color=000000" alt="" /></a>
+                    <a href=""><img className="ICONS" src="https://img.icons8.com/?size=100&id=BrU2BBoRXiWq&format=png&color=000000" alt="" /></a>
+                    <a href=""><img className="ICONS" src="https://img.icons8.com/?size=100&id=3veRWJpxPPDH&format=png&color=000000" alt="" /></a>
+                
+                </div>
+                <div>
+                    
                 </div>
             </div>
         </div>
-        <div className="copyright">
-            <p className="text-white">Copyrihgt <img className="text-white" src="https://img.icons8.com/?size=100&id=cBtsUdDPCDm3&format=png&color=000000" alt="" /> 2024 VICTOR. all rights reserved.</p>
-        </div>
-    </div>
-       </>
-    
     )
 }
 
