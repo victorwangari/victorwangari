1a2f97be9a1da59462fb5440c67f8d6893445ccc
root <root@VICKY-DESKTOP-2CS1R7B.localdomain>
2025-01-17 12:34:14 +0300

    update app

--- src/components /hero.jsx
+++ src/components /hero.jsx
@@ -7,9 +7,9 @@ function Hero() {
     return(
         <div className="hero-section  bg-black">
             <div className="hero">
-                <h1 className="hero-heading text-white">Hi, Its <span className="vic">VIC</span>TOR</h1>
+                <h1 className="hero-heading text-white">Hi, Its <span className="dev">VIC</span>TOR</h1>
                 <h2 className="text-white hero-h2">
-                    I'm a Web <span className="vic">Developer</span>
+                    I'm a Web <span className="dev">Developer</span>
                 </h2>
                 <p className="hero-text text-white">I'm a passionate software engineer with a strong foundation in building user-centric applications. I graduated from Moringa School, where I honed my skills in modern web technologies and best practices. With a focus on creating clean, efficient code and seamless user experiences, I'm always eager to tackle new challenges and continue growing in the tech industry.</p>
                 <div>
